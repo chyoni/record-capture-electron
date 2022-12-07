2646b4af46d4380df852fb731a95ebe7339d2705
choi chiwon <choichiwon@chois-MacBook-Pro.local>
2022-12-07 10:34:31 +0900

    #03 Record Me

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 
 ### #01 Init
+
+### #02 Menu
+
+- 메뉴에서는 system-define standard menu가 많은데 그 녀석을 사용할 수 있는게 role이라는 키워드이다.
+
+### #03 Record Me
+
+- video tag를 이용하여 eletron으로 접근
--- main.js
+++ main.js
@@ -1,11 +1,65 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu, shell } = require("electron");
 const path = require("path");
 const isDev = process.env.NODE_ENV !== "production";
+
+const menuItems = [
+  {
+    label: "Menu",
+    submenu: [
+      {
+        label: "Learn More",
+        click: () => shell.openExternal("https://www.electronjs.org"),
+      },
+      {
+        label: "About",
+      },
+      {
+        type: "separator",
+      },
+      {
+        label: "Exit",
+        click: () => app.quit(),
+      },
+    ],
+  },
+  {
+    label: "Standard Menus",
+    submenu: [
+      {
+        role: "minimize",
+      },
+      {
+        role: "close",
+      },
+    ],
+  },
+  {
+    label: "New Windows",
+    submenu: [
+      {
+        label: "Record me",
+        click: () => {
+          const win2 = new BrowserWindow({
+            width: 500,
+            height: 500,
+            show: false,
+          });
+          win2.webContents.openDevTools();
+          win2.loadFile("recordMe.html");
+          win2.once("ready-to-show", () => win2.show());
+        },
+      },
+    ],
+  },
+];
+
+const menu = Menu.buildFromTemplate(menuItems);
+Menu.setApplicationMenu(menu);
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 1000,
-    height: 1000,
+    height: 600,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
