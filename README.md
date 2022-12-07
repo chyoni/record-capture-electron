2a37681942cccb2ca9560c09cab8114c24913c62
choi chiwon <choichiwon@chois-MacBook-Pro.local>
2022-12-07 09:21:42 +0900

    #01 Init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Recording and Capture App
+
+
+### #01 Init
---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const isDev = process.env.NODE_ENV !== "production";
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 1000,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+  if (isDev) win.webContents.openDevTools();
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
